6d4364e2414acb85a538e27a8451234bdd690a43
beProsto <lavionperavion@gmail.com>
2023-01-01 04:52:34 +0100

    kayboard and mouse input handling

---
+++ include/env/input.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// Polling every value all at once
+struct input_t
+{
+  int32_t position_x;
+  int32_t position_y;
+  int32_t delta_x;
+  int32_t delta_y;
+  int32_t scroll_x;
+  int32_t scroll_y;
+  bool keys[256];
+  bool buttons[5];
+};
+
+// Input getter
+void
+env_ig(struct input_t* ptr);
--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include <env/input.h>
 #include <env/memory.h>
 #include <env/print.h>
+#include <stdbool.h>
 
 void
 main()
@@ -14,4 +16,28 @@ main()
     numberToReturn,
     'a',
     2.5);
+}
+
+struct input_t input = {};
+
+void
+loop()
+{
+  env_ig(&input);
+
+  printf("Mouse is on: %d : %d\n", input.position_x, input.position_y);
+  printf("Mouse delta: %d : %d\n", input.delta_x, input.delta_y);
+  printf("Mouse wheel: %d : %d\n", input.scroll_x, input.scroll_y);
+  printf("Mouse buttons: [%d / %d / %d | %d / %d]\n",
+         input.buttons[0],
+         input.buttons[1],
+         input.buttons[2],
+         input.buttons[3],
+         input.buttons[4]);
+  printf("Keyboard: W[%d] S[%d] A[%d] D[%d] Space[%d]\n",
+         input.keys[87],
+         input.keys[83],
+         input.keys[65],
+         input.keys[68],
+         input.keys[32]);
 }
